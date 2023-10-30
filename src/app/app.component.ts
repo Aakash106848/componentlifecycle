2aa1053daa05b99e3d005e827408bb824de6eacc
Aakash106848 <aakash.b@gavstech.com>
2023-10-30 10:13:44 +0530

    Project Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, Component, DoCheck, OnDestroy, OnInit,AfterViewChecked,AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit,OnDestroy,DoCheck,AfterContentChecked,AfterContentInit,AfterViewInit,AfterViewChecked {
   title = 'componentlifecycle';
+  firstName:string='';
+  lastName:string='';
+  ngOnInit(): void {//phase 1 - initialization phase
+    //write the code to connect to backend and fetch data
+      console.log('Component initialized! at '+new Date());
+  }
+  ngOnDestroy(): void {
+    //write the code to connect to backend and save data
+      console.log('Component destroyed! at'+new Date());
+      localStorage.clear();
+
+  }
+  ngDoCheck(): void {//phase 2 - validation phase ,executed whenever the view changes
+      console.log('Overall Verification/ Validation Started');
+  }
+  ngAfterContentChecked(): void {//step 4  exected after view changes everytime
+      console.log('Content verified / Checked!');
+  }
+  ngAfterContentInit(): void {//phase 3 - After initialization phase
+      console.log('Content initialization COmpleted! ');
+  }
+  ngAfterViewChecked(): void { //step 6 executed everytime the view changes
+      console.log('View Check Completed!');
+  }
+  ngAfterViewInit(): void {//step 5  executed once after initialization of the component occurs
+      console.log('View initialization Completed!');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {OnInit,OnDestroy} from '@angular/core';
+import { DoCheck,AfterContentChecked } from '@angular/core';
+import { AfterContentInit } from '@angular/core';
+import { AfterViewChecked } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
